cd15f542378c2db975f87be21880f25c6924bcd0
provToddFoxford <todd.foxford@providence.org>
2019-05-13 13:58:28 -0700

    add regex to remove ugly chars in jest response

--- SilentReporter.js
+++ SilentReporter.js
@@ -13,6 +13,8 @@ function logFailedTestResultDetail(trd) {
     title,
     status,
   } = trd;
+
+  return trd;
 }
 
 function logFailedTestResult(tr) {
@@ -27,10 +29,14 @@ function logFailedTestResult(tr) {
   trHeader.testResults = [];
   for (let i = 0; i < tr.testResults.length; i++) {
     const trd = tr.testResults[i];
-    if (trd && trd.status !== 'passed') {
+    if (trd) { //  && trd.status !== 'passed') {
       const reTrd = logFailedTestResultDetail(trd);
       trHeader.testResults.push(reTrd);
     }
+  }
+
+  if (trHeader.testResults.length === 0) {
+    trHeader.testResults = null;
   }
 
   return trHeader;
@@ -56,6 +62,10 @@ class SilentReporter {
     this.stdio.close();
   }
 
+  // I remove all of the crap [22m strings that Jest returns
+  // and return a nicely formatted JSON response for each test
+  // one on each line for Splunk.
+
   onTestResult(test, testResult) {
     if (this.useDots) {
       this.stdio.logInline('.');
@@ -63,10 +73,10 @@ class SilentReporter {
 
     if (!testResult.skipped) {
       if (testResult.failureMessage) {
-        // this.stdio.log('\n' + testResult.failureMessage);
-
         const jsonFailed = logFailedTestResult(testResult);
-        this.stdio.log('\n', + JSON.toString(jsonFailed));ß
+        const failedStr = JSON.stringify(jsonFailed);
+        const niceStr = failedStr.replace(/\[\d+m/g, '')
+        this.stdio.log('\n\n' + niceStr);
       }
       const didUpdate = this._globalConfig.updateSnapshot === 'all';
       const snapshotStatuses = helpers.getSnapshotStatus(
@@ -76,8 +86,6 @@ class SilentReporter {
       snapshotStatuses.forEach(this.stdio.log);
     }
   }
-
-
 }
 
 module.exports = SilentReporter;
